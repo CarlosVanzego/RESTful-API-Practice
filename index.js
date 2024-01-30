4d313ee3b7aa556cb1c08e1303a306469e119b8b
Your Name <carlosvanzego96@gmail.com>
2024-01-29 23:57:17 -0500

    code comments commit

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+// Importing the Express framework
 const express = require('express');
+// Creating an instance of the Express application
 const app = express();
+// Defining the port number
 const PORT = 8080;
 
-app.use( express.json() )
+// Middleware to parse incoming JSON data
+app.use(express.json());
 
-app.get('/' , (req, res) => {
-    console.log('HOME')
+// Route for the home page
+app.get('/', (req, res) => {
+    console.log('HOME');
+    // Sending a basic HTML response for the home page
     res.send(`
     <!DOCTYPE html>
     <html>
@@ -20,64 +26,71 @@ app.get('/' , (req, res) => {
 `);
 });
 
-app.get('/tshirt' , (req, res) => {
-    console.log('tshirt')
+// Route for retrieving a t-shirt
+app.get('/tshirt', (req, res) => {
+    console.log('tshirt');
+    // Sending a JSON response with t-shirt details
     res.status(200).send({
         tshirt: '👕',
         size: 'large'
-     })
+     });
 });
 
-app.get('/pants' , (req, res) => {
-    console.log('pants')
+// Route for retrieving pants
+app.get('/pants', (req, res) => {
+    console.log('pants');
+    // Sending a JSON response with pants details
     res.status(200).send({
         pants: '👖',
         size: 'large'
-     })
+     });
 });
 
+// Route for adding a logo to a t-shirt
 app.post('/tshirt/:id', (req, res) => {
-    // console.log('request', req)
-    // console.log('response', res)
-    console.log('tshirt id')
-    console.log(req.body)
-    console.log('req.params'. req.params)
+    console.log('tshirt id');
+    // Logging the request body and parameters
+    console.log(req.body);
+    console.log('req.params', req.params);
+    // Extracting id and logo from request parameters and body
     const { id } = req.params;
     const { logo } = req.body;
 
+    // Checking if logo is provided, if not, sending an error response
     if (!logo) {
-        res.status(418).send({ message: 'We need a logo!' })
+        res.status(418).send({ message: 'We need a logo!' });
     }
 
+    // Sending a response confirming the t-shirt customization
     res.send({
         tshirt: `👕 with your ${logo} and ID of ${id}`,
     });
 });
 
+// Route for adding a logo to pants
 app.post('/pants/:id', (req, res) => {
-    console.log('pants id')
+    console.log('pants id');
+    // Extracting id and logo from request parameters and body
     const { id } = req.params;
     const { logo } = req.body;
 
+    // Checking if logo is provided, if not, sending an error response
     if (!logo) {
-        res.status(418).send({ message: 'We need a logo!' })
+        res.status(418).send({ message: 'We need a logo!' });
     }
+
+    // Sending a response confirming the pants customization
     res.send({
         pants: `👖 with your ${logo} and ID of ${id}`,
     });
-   
 });
 
-    
-
-// http://0.0.0.0:8080/
-// http://localhost:8080/
-
-app.listen(PORT, function (err){
-    if (err){
-        console.log('err', err)
+// Starting the server
+app.listen(PORT, function (err) {
+    // Handling server start-up errors
+    if (err) {
+        console.log('err', err);
+    } else {
+        console.log('---server running on', PORT);
     }
-    else{
-        console.log('---server runnning on', PORT)
-    }
-})
+});
